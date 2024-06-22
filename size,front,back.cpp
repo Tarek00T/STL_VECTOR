8c4438bbde549cb2bd79c667eb6d602bedcdde47
Md Tarek Miah <140520331+Tarek00T@users.noreply.github.com>
2024-06-22 12:20:31 +0600

    Add files via upload

---
+++ size,front,back.cpp
@@ -0,0 +1,27 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+void solve()
+{
+
+    vector<int>v1;
+    v1.push_back(10);
+    v1.push_back(20);
+    v1.push_back(30);
+    v1.push_back(40);
+    v1.push_back(50);
+    v1.push_back(60);
+    cout<<"SIZE : "<<v1.size()<<endl;
+    cout<<"FRONT : "<<v1.front()<<endl;
+    cout<<"BACK : "<<v1.back()<<endl;
+}
+int main()
+{
+    int t=1;
+    //cin >>t;
+    while(t--)
+    {
+        solve();
+    }
+}
+
